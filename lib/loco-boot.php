167e4be2e9dd31bd88d6ff46cad6f87038ed1d22
timwhitlock <timwhitlock@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-31 16:24:15 +0000

    published 1.1.0
    git-svn-id: https://plugins.svn.wordpress.org/loco-translate/trunk@748841 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/loco-boot.php
+++ lib/loco-boot.php
@@ -8,7 +8,7 @@ abstract class Loco {
     /** plugin namespace */
     const NS = 'loco-translate';
     
-    const VERSION = '1.0.0';
+    const VERSION = '1.1.0';
     const CAPABILITY = 'manage_options';
     
     /* whether to enable APC cache */
--- loco.php
+++ loco.php
@@ -4,7 +4,7 @@ Plugin Name: Loco Translate
 Plugin URI: http://wordpress.org/extend/plugins/loco-translate
 Description: Translate Wordpress plugins and themes directly in your browser
 Author: Tim Whitlock
-Version: 1.1.0-dev
+Version: 1.1.0
 Author URI: http://localise.biz/
 */
 
